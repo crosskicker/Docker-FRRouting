666e8f4167721a09af9f0cc84561b3eb276b8299
crosskicker <alban.personnaz@gmail.com>
2024-11-02 13:45:58 +0100

    export project version

--- automatic/erase.py
+++ automatic/erase.py
@@ -1,13 +1,13 @@
 import subprocess
 
-# Commande pour arrêter tous les conteneurs en cours d'exécution
+
 try:
     subprocess.run("docker stop $(docker ps -q)", shell=True, check=True)
     print("Tous les conteneurs en cours d'exécution ont été arrêtés.")
 except subprocess.CalledProcessError:
     print("Aucun conteneur à arrêter ou erreur lors de l'arrêt.")
 
-# Commande pour supprimer tous les conteneurs arrêtés
+
 try:
     subprocess.run("docker rm $(docker ps -a -q -f status=exited)", shell=True, check=True)
     print("Tous les conteneurs arrêtés ont été supprimés.")
--- automatic/export.py
+++ automatic/export.py
@@ -1,4 +1,20 @@
 # export.py
 # to export the project and run it on an other machine
+import tarfile
+import argparse
 
-#TODO
+
+# List of files to include in the archive
+files_to_include = ["docker-compose.yml", "volumes/"]
+
+def tar_project(name):
+    # Create a tar.gz archive and add each file
+    with tarfile.open(f"{name}.tar.gz", "w:gz") as tarf:
+        for file in files_to_include:
+            tarf.add(file)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=" To export the network project in tar format")
+    parser.add_argument("-o", "--output", help="name of tar project", type=str, default=4)
+    args = parser.parse_args() 
+    tar_project(args.output)
