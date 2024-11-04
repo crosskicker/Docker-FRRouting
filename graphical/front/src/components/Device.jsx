4cd6f70423161a2581e88fa4c5ae795454d7e6b6
crosskicker <alban.personnaz@gmail.com>
2024-11-04 16:31:01 +0100

    v graphical in progress

--- graphical/front/src/components/Device.jsx
+++ graphical/front/src/components/Device.jsx
@@ -3,12 +3,19 @@ import React, { useState } from 'react';
 function Device({ name }) {
   // État pour contrôler la visibilité de la liste déroulante
   const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [routerList, setRouterList] =  useState([]);
+  const [nbRouter, setNbRouter] = useState(0);
 
   // Fonction pour basculer la visibilité de la liste déroulante
   const toggleDropdown = () => {
     setDropdownVisible(!isDropdownVisible);
   };
 
+  const addRouter = () => {
+    setNbRouter((prevNbRouter) => prevNbRouter + 1);
+    setRouterList([...routerList, `router${nbRouter}` ]);
+    console.log(routerList);
+  };
   return (
     <div className="relative">
       {/* Élément cliquable */}
@@ -20,12 +27,12 @@ function Device({ name }) {
       </div>
 
       {/* Liste déroulante conditionnelle */}
+      {/* Créer un nouveau composant pour ce qui est invisible car cette partie
+      est différentes pour les autres */}
       {isDropdownVisible && (
         <div className="flex top-full mt-2 w-40 bg-gray-800 text-white p-2 rounded shadow-lg">
           <ul className="flex flex-col space-y-2">
-            <li className="hover:bg-gray-700 p-2 rounded">Option 1</li>
-            <li className="hover:bg-gray-700 p-2 rounded">Option 2</li>
-            <li className="hover:bg-gray-700 p-2 rounded">Option 3</li>
+            <li className="hover:bg-gray-700 p-2 rounded cursor-pointer" onClick={addRouter}>Router FRR</li>
           </ul>
         </div>
       )}
