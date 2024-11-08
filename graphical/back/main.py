e7c799a9bfd0e6a1a2924ccd180a37999e9a2a54
crosskicker <alban.personnaz@gmail.com>
2024-11-08 15:40:26 +0100

    DnD beta

--- graphical/back/main.py
+++ graphical/back/main.py
@@ -1,9 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 
 app = Flask('yourapplication')
 
 CORS(app)
+
+def write_data(data):
+    try:
+        # Open template
+        with open("myObj.json", "r") as file:
+            myObj = json.load(file)
+    except FileNotFoundError:
+        print("Erreur : File doesn't exists.")
+    except IOError:
+        print("Erreur : Error : Can't read the file.")
+
+    myObj["netDevice"].append(data)
+
+    try:
+    #generate new json
+        with open("myObj.json", "w") as file:
+            json.dump(myObj, file, indent=4)
+    except IOError:
+        print(f"Error : Can't write the file.")
 
 @app.route('/')
 def home():
@@ -17,7 +37,7 @@ def addToBoard():
             "received_data": data  # Inclure les données reçues pour confirmation
         }
     print(data)
-
+    write_data(data)
     return jsonify(response), 200
 
 # Lancer l'application en mode développement
